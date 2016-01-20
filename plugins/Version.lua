78ed2baa2c2e0a5a2b5963ebed376eb538c985ab
redstormbot <redstormbot7@gmail.com>
2016-01-20 19:49:23 +0330

    Update Version.lua

--- plugins/Version.lua
+++ plugins/Version.lua
@@ -3,7 +3,6 @@ do
 function run(msg, matches)
 return [[ 
 redstorm Robot Ver 4.2
-website : https://arashinfernal.epage.ir
 Our Channel : @redstormch
 Our code is open source but !
 remember : copy right!
